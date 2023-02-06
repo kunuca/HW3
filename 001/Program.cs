d07bf2286128a1a2cb017a412865771b0599549f
Tatiana Kustova <kunuca@mail.ru>
2023-02-06 17:04:18 +0400

    001

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число");
+int step=int.Parse(Console.ReadLine());
+for (int count=1; count <=step; count++)
+{
+    Console.WriteLine($"Куб числа {count} равен {Math.Pow(count, 3)} ");
+}
