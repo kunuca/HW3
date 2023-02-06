aca16bd782415f66a2330c2ca22b160eb703f3d3
Tatiana Kustova <kunuca@mail.ru>
2023-02-06 21:26:52 +0400

    003

---
+++ 003/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число");
+int k=int.Parse(Console.ReadLine());
+int sum=1;
+for (int i=1; i<=k; i++) sum=sum*i;
+Console.WriteLine($"Произведение чисел от 1 до {k} равно {sum}");
