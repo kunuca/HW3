d633dbbafbb5be7f0b9dde0bce74b6fe9831240e
Tatiana Kustova <kunuca@mail.ru>
2023-02-06 17:15:22 +0400

    002

---
+++ 002/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int n=int.Parse(Console.ReadLine());
+int sum=0;
+while (n>0)
+{
+    int k=n%10;
+    sum=sum+k;
+    n=n/10;
+}
+Console.WriteLine(sum);
