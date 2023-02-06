bf799ea3fb623dbd64902ea7627be039439007fd
Tatiana Kustova <kunuca@mail.ru>
2023-02-06 22:02:33 +0400

    004

---
+++ 004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число");
+int step=int.Parse(Console.ReadLine());
+double q=1;
+
+for (int count=1; count <=step; count++)
+{
+    q=Math.Pow(count, 3);
+    if (q%2==0) Console.WriteLine(q);
+    
+}
